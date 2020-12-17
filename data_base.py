0eea7a2d945a3fa2991acc5bbdd3a793c79fd56a
Reshko <kolpak.al6@yandex.ru>
2020-12-17 22:35:06 +0300

    Начал работать с логикой

--- data_base.py
+++ data_base.py
@@ -6,13 +6,23 @@ __connection = None
 def get_connection():
     global __connection
     if __connection is None:
-        __connection = sqlite3.connect('quest_answ.db')
+        __connection = sqlite3.connect('questions.db')
     return __connection
 
 
-def get_question():
+def get_question(id_quest: int):
     conn = get_connection()
     c = conn.cursor()
-    c.execute('SELECT question FROM questions WHERE id = 1')
+    c.execute('SELECT question FROM main_questions WHERE id =? ', (id_quest,))
     (res,) = c.fetchone()
     return res
+
+
+def get_answers(id_quest: int):
+    conn = get_connection()
+    c = conn.cursor()
+    c.execute('SELECT answer_1,answer_2,answer_3,answer_4 FROM main_questions WHERE id =?', (id_quest,))
+    (res,) = c.fetchall()
+    return res
+
+
--- main.py
+++ main.py
@@ -39,88 +39,65 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-GENDER, PHOTO, LOCATION, BIO = range(4)
+FIRST_Q, SECOND_Q, THREE_Q, FOUR_Q = range(4)
 
 
 def start(update: Update, context: CallbackContext) -> int:
-    reply_keyboard = [['Да.Каждые выходные', 'Иногда', 'Нет']]
+    reply_keyboard = [['Подобрать автомобиль']]
+    user = update.message.from_user
 
-    # update.message.reply_text(
-    #     'Привет! Помогу тебе выбрать автомобиль '
-    #     'Вы планиурете поездки на дачу ?\n\n',
-    #     reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
-    # )
     update.message.reply_text(
-        data_base.get_question(),
+        f'Привет {user.first_name}. Помогу тебе подобрать автомобиль,согласно твоим предпочтениям.',
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
     )
-
-    return GENDER
+    return FIRST_Q
 
 
 def gender(update: Update, context: CallbackContext) -> int:
-    user = update.message.from_user
-    logger.info("Gender of %s: %s", user.first_name, update.message.text)
     update.message.reply_text(
-        'I see! Please send me a photo of yourself, '
-        'so I know what you look like, or send /skip if you don\'t want to.',
+        'Инномарка или БУ',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+    return SECOND_Q
+
+
+def pender(update: Update, context: CallbackContext) -> int:
+    #logger.info("Gender of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text(
+        'Состояние 2',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+    if update.message.text == '3':
+        print("3")
+
+        return THREE_Q
+    elif update.message.text == '4':
+        print("4")
+
+        return FOUR_Q
+    else:
+        print('asdasd')
+
+
+
+def fender(update: Update, context: CallbackContext) -> int:
+    #logger.info("Gender of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text(
+        'Вы попали в фендер',
         reply_markup=ReplyKeyboardRemove(),
     )
 
-    return PHOTO
+    return ConversationHandler.END
 
-
-def photo(update: Update, context: CallbackContext) -> int:
-    user = update.message.from_user
-    photo_file = update.message.photo[-1].get_file()
-    photo_file.download('user_photo.jpg')
-    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
+def blender(update: Update, context: CallbackContext) -> int:
+    #logger.info("Gender of %s: %s", user.first_name, update.message.text)
     update.message.reply_text(
-        'Gorgeous! Now, send me your location please, ' 'or send /skip if you don\'t want to.'
+        'Вы попали в блендер',
+        reply_markup=ReplyKeyboardRemove(),
     )
-
-    return LOCATION
-
-
-def skip_photo(update: Update, context: CallbackContext) -> int:
-    user = update.message.from_user
-    logger.info("User %s did not send a photo.", user.first_name)
-    update.message.reply_text(
-        'I bet you look great! Now, send me your location please, ' 'or send /skip.'
-    )
-
-    return LOCATION
-
-
-def location(update: Update, context: CallbackContext) -> int:
-    user = update.message.from_user
-    user_location = update.message.location
-    logger.info(
-        "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
-    )
-    update.message.reply_text(
-        'Maybe I can visit you sometime! ' 'At last, tell me something about yourself.'
-    )
-
-    return BIO
-
-
-def skip_location(update: Update, context: CallbackContext) -> int:
-    user = update.message.from_user
-    logger.info("User %s did not send a location.", user.first_name)
-    update.message.reply_text(
-        'You seem a bit paranoid! ' 'At last, tell me something about yourself.'
-    )
-
-    return BIO
-
-
-def bio(update: Update, context: CallbackContext) -> int:
-    user = update.message.from_user
-    logger.info("Bio of %s: %s", user.first_name, update.message.text)
-    update.message.reply_text('Thank you! I hope we can talk again some day.')
 
     return ConversationHandler.END
+
 
 
 def cancel(update: Update, context: CallbackContext) -> int:
@@ -146,13 +123,10 @@ def main() -> None:
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            GENDER: [MessageHandler(Filters.regex('^(Да.Каждые выходные|Иногда|Нет)$'), gender)],
-            PHOTO: [MessageHandler(Filters.photo, photo), CommandHandler('skip', skip_photo)],
-            LOCATION: [
-                MessageHandler(Filters.location, location),
-                CommandHandler('skip', skip_location),
-            ],
-            BIO: [MessageHandler(Filters.text & ~Filters.command, bio)],
+            FIRST_Q: [MessageHandler(Filters.text, gender)],
+            SECOND_Q: [MessageHandler(Filters.text, pender)],
+            THREE_Q: [MessageHandler(Filters.text,fender)],
+            FOUR_Q: [MessageHandler(Filters.text,blender)]
         },
         fallbacks=[CommandHandler('cancel', cancel)],
     )
